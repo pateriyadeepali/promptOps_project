36b0f910f48c2c1f299093cc188b573804b72c7b
deepali <pateriyadeepali1008@gmail.com>
2024-08-31 14:46:36 +0530

    second

---
+++ template/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operator = request.form['operator']
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = 'Error: Division by zero!'
+        else:
+            result = 'Invalid operator!'
+
+        return render_template('index.html', result=result)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
